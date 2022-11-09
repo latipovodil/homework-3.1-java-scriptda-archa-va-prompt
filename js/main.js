567786efc500a95950ae4a94345cd4be5ec9b418
Odilbek <113532560+odilbekl@users.noreply.github.com>
2022-11-09 16:23:45 +0500

    java script

---
+++ js/main.js
@@ -0,0 +1,107 @@
+alert("Begonalar va yosh bolalar kirishi taqiqlanadi")
+
+var username = prompt("Ismingiz");
+
+let age = prompt("Yoshingiz")*1;
+
+
+let teng = age;
+
+let yosh = teng;
+
+
+var salom = username;
+
+document.write(
+ "Assalom alaykum Xush kelibsiz   ",
+ salom,
+ " / / / / / / / / / / / "
+)
+
+
+
+
+
+
+if (teng > 17) {
+
+
+  document.write(
+    "----Saytga kirishingiz mumkin" , "----Chunki siz 18 yoshga to'lgansiz", "----Chapdagi link orqali"
+    )
+    
+
+  
+} else {
+  document.write(
+    "Siz 18 ga to'lmagansiz", "----Siz kirmang deb sayt boshida eslatilgan", "-----kattaroq kishi bilan kirishingiz mumkin"
+    )
+    
+}
+
+document.write(
+ "------------------------------------------------------", salom," - sizga qo'shimcha yordamlar kerak bo'lsa +998 99 999 99 99"
+)
+
+document.write(
+  "---------------archa consolda"
+)
+
+
+
+
+console.log(
+  "             *"
+)
+console.log(
+  "            ***"
+)
+console.log(
+  "           *****"
+)
+console.log(
+  "          *******"
+)
+console.log(
+  "         *********"
+)
+console.log(
+  "        ***********"
+)
+console.log(
+  "       *************"
+)
+console.log(
+  "      ***************"
+)
+console.log(
+  "     *****************"
+)
+console.log(
+  "    *******************"
+)
+console.log(
+  "   *********************"
+)
+console.log(
+  "  ***********************"
+)
+console.log(
+  " *************************"
+)
+console.log(
+  "***************************"
+)
+
+console.log(
+  "          *****"
+)
+console.log(
+  "          *****"
+)
+console.log(
+  "          *****"
+)
+console.log(
+  "          *****"
+)
